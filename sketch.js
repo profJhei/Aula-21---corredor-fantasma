b1f34e085181652788ce9baaac7e0564df818eab
profJhei <115741264+profJhei@users.noreply.github.com>
2022-11-24 13:41:53 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,57 @@
+  
+function preload(){
+  
+}
+
+function setup() {
+  createCanvas(600,600);
+  
+}
+
+
+function draw() {
+  background(255);
+  
+  if (gameState === "play") {
+    
+    if(keyDown("")){
+  
+      // escreva o código para mover para a esquerda quando a seta para a esquerda for pressionada
+    }
+    if(keyDown("")){
+  
+    
+      // escreva o código para mover para a esquerda quando a seta para a direita for pressionada
+      
+    }
+    if(keyDown("")){
+  
+   
+      // escreva o código para mover para cima quando a tecla espaço for pressionada
+      
+    }
+  
+  
+   
+  //escreva uma condição para a torre de rolagem infinita
+    
+  spawnDoors();
+
+  
+  //escrever um código para fazer o climbersGroup (grupo de escaladores) colidir com o fantasma alterar a velocidade do fantasma  
+  //escreva um código para fazer o invisibleBlockGroup (grupo de bloco invisível) colidir com o fantasma, destruir o fantasma e mudar o estado de jogo para end.
+  
+  drawSprites();
+}
+  if (gameState === "end"){
+    
+  }
+}
+
+function spawnDoors()
+{
+  //escreva o código aqui para gerar as portas
+  
+  
+}
+
